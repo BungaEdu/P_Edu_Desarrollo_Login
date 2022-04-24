b90b531899176123f265a2207d7c8b3dc9fffc1d
BungaEdu <eduardoflores.9595@gmail.com>
2022-04-25 00:30:00 +0200

    casa 25/04 - Procedimiento petición num FAIL

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int num=0;
+        Procedimiento.preguntaNum(num);
+        int arrayNum [] = new int [1];
+        System.out.println(arrayNum[0]+1); //Por qué me da 5 si me debería dar 6
     }
-    //Prueba commit Asus
-    //Prueba commit Acer
 }
---
+++ src/Procedimiento.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Procedimiento {
+    public static void preguntaNum(int num) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Dime número: ");
+        int arrayNum [] = new int [1];
+        arrayNum[0] = sc.nextInt();
+    }
+}
